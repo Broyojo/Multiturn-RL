954506890d39065884e66ddec9bce79c8f39d5bd
Broyojo <dha@xoba.com>
2025-05-01 16:25:24 -0400

    added swesmith data processing

--- experiments/prepare_swebench_data.py
+++ experiments/prepare_swebench_data.py
@@ -1,15 +1,95 @@
 import os
 
 import docker
-from datasets import Dataset
-from swebench import build_instance_images
+from datasets import Dataset, load_dataset
+from swebench import build_instance_images, run_threadpool
 from swebench.harness.constants import LATEST
 from swebench.harness.utils import load_swebench_dataset
+from swesmith.build_repo.download_images import (
+    SWEFT_ORG,
+    TAG,
+    get_docker_hub_login,
+    get_docker_repositories,
+    get_dockerhub_token,
+)
 
 client = docker.from_env(max_pool_size=1024)
 
+SYSTEM_PROMPT = """
+You are a software engineering AI agent whose task is to resolve issues in Github repositories. You will be given an issue as well as access to a TTY into a Docker container with the repository installed. You can access the terminal to this docker container like so:
 
-def build(dataset, split):
+<terminal>echo hello world
+</terminal>
+<output>
+hello world
+~ $ </output>
+
+Your job is to solve the issue given by the user by modifying the code in the repository.
+
+Make sure to alternate between thinking and doing terminal actions. Your final response to the user should be in <answer></answer> tags. Here is an example template:s
+
+<think>[your thinking here]</think>
+<terminal>[your terminal input here]</terminal>
+<think>[your thinking here]</think>
+<terminal>[your terminal input here]</terminal>
+...
+<think>[your thinking here]</think>
+<answer>[your final answer here]</answer>
+""".strip()
+
+
+def download_swesmith_images():
+    username, password = get_docker_hub_login()
+    token = get_dockerhub_token(username, password)
+
+    # Get list of swesmith repositories
+    repos = get_docker_repositories(SWEFT_ORG, token)
+    repos = [r for r in repos if r["name"].startswith("swesmith")]
+
+    for idx, r in enumerate(repos):
+        print("-", r["name"])
+        if idx == 4:
+            print(f"(+ {len(repos) - 5} more...)")
+            break
+
+    def download(r):
+        client.images.pull(f"{SWEFT_ORG}/{r['name']}:{TAG}")
+        # Rename images via tagging
+        new_name = f"{r['name'].replace('_1776_', '__')}:{TAG}"
+        client.images.get(f"{SWEFT_ORG}/{r['name']}:{TAG}").tag(new_name)
+
+    success, failure = run_threadpool(
+        download, [(r,) for r in repos], max_workers=os.cpu_count()
+    )
+
+    return success, failure
+
+
+def build_swesmith_train(ds):
+    success, _ = download_swesmith_images()
+    verl_ds = []
+    for i, d in enumerate(ds):
+        verl_ds.append(
+            {
+                "data_source": "swesmith",
+                "prompt": [
+                    {"role": "system", "content": SYSTEM_PROMPT},
+                    {"role": "user", "content": d["problem_statement"]},
+                ],
+                "ability": "code",
+                "reward_model": {"style": "rule", "ground_truth": ""},
+                "extra_info": {
+                    "split": "train",
+                    "index": i,
+                    "docker_image": d["image_name"],
+                    "base_commit": d["base_commit"],
+                },
+            }
+        )
+    return Dataset.from_list(verl_ds)
+
+
+def build_swebench_test(dataset, split):
     successful, _ = build_instance_images(
         client=client, dataset=dataset, max_workers=8, tag=LATEST
     )
@@ -26,7 +106,7 @@ def build(dataset, split):
             {
                 "data_source": "swebench",
                 "prompt": [
-                    {"role": "system", "content": ""},
+                    {"role": "system", "content": SYSTEM_PROMPT},
                     {"role": "user", "content": d["problem_statement"]},
                 ],
                 "ability": "code",
@@ -34,6 +114,7 @@ def build(dataset, split):
                 "extra_info": {
                     "split": split,
                     "index": i,
+                    "base_commit": d["base_commit"],
                     "docker_image": image_map[d["instance_id"]].instance_image_key,
                 },
             }
@@ -46,14 +127,17 @@ def main():
     OUT = "./data/swebench/"
     os.makedirs(OUT, exist_ok=True)
 
-    # train_dataset = build(
-    #     load_swebench_dataset("princeton-nlp/SWE-bench", "train"), split="train"
-    # )
-    test_dataset = build(
+    train_dataset = load_dataset("SWE-bench/SWE-smith", split="train")
+    train_dataset = train_dataset.filter(
+        lambda e: len(e["problem_statement"]) > 0, num_proc=16
+    )
+    train_dataset = build_swesmith_train(train_dataset)
+
+    test_dataset = build_swebench_test(
         load_swebench_dataset("princeton-nlp/SWE-bench", "test"), split="test"
     )
 
-    # train_dataset.to_parquet(os.path.join(OUT, "train.parquet"))
+    train_dataset.to_parquet(os.path.join(OUT, "train.parquet"))
     test_dataset.to_parquet(os.path.join(OUT, "test.parquet"))
 
 
--- experiments/swesmith.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-t = 0
-ds = load_dataset("SWE-bench/SWE-smith")
-filtered = ds.filter(lambda e: len(e["problem_statement"]) > 0, num_proc=16)
-print(filtered)
-print(filtered["train"][69]["problem_statement"])
