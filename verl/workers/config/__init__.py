f0c02d420d99e8260c18cbd9bbf56dc8b69f6f28
Mengyang Liu <mliu444@gatech.edu>
2025-08-03 04:55:47 +0000

    Update verl

--- verl/workers/config/__init__.py
+++ verl/workers/config/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Bytedance Ltd. and/or its affiliates
+# Copyright 2025 Bytedance Ltd. and/or its affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,3 +10,12 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .critic import *  # noqa
+from .actor import *  # noqa
+from .engine import *  # noqa
+from .optimizer import *  # noqa
+from . import actor, critic, engine, optimizer
+
+__all__ = actor.__all__ + critic.__all__ + engine.__all__ + optimizer.__all__
